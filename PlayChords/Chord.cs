0669cab77de3bcd0dc863f64e731e1c1dc27e9ff
Dries de Groot <GrootD@wigo4it.nl>
2022-02-27 22:17:40 +0100

    Meer akkoorden

--- PlayChords/Chord.cs
+++ PlayChords/Chord.cs
@@ -5,7 +5,7 @@ namespace PlayChords
 {
     public class Chord
     {
-        readonly List<string> allNotes = new List<string>(12);
+        readonly List<string> allNotes = new(12);
 
         public Chord()
         {
@@ -23,35 +23,76 @@ namespace PlayChords
             allNotes.Add("As");
         }
 
-        public string MajeurAkkoord(string note, bool dominant)
+
+        public string MajeurAkkoord(string baseNote)
         {
-            var index1 = allNotes.IndexOf(note);
-            if (dominant)
-            {
-                Math.DivRem((index1 + 4), 12, out int index2);
-                Math.DivRem((index2 + 3), 12, out int index3);
-                Math.DivRem((index3 + 2), 12, out int index4);
-                return $"{note} - {allNotes[index2]} - {allNotes[index3]} - {allNotes[index4]}";
-            }
-            else
-            {
-                Math.DivRem((index1 + 4), 12, out int index2);
-                Math.DivRem((index2 + 3), 12, out int index3);
-                return $"{note} - {allNotes[index2]} - {allNotes[index3]}";
-            }
+            var index1 = allNotes.IndexOf(baseNote);
+            Math.DivRem((index1 + 4), 12, out int index2);
+            Math.DivRem((index2 + 3), 12, out int index3);
+            return $"{baseNote} - {allNotes[index2]} - {allNotes[index3]}";
         }
 
-        public string Doe()
-        {
-            return "";
-        }
 
-        public string MineurAkkoord(string note)
+
+        public string MineurAkkoord(string baseNote)
         {
-            var index1 = allNotes.IndexOf(note);
+            var index1 = allNotes.IndexOf(baseNote);
             Math.DivRem((index1 + 3), 12, out int index2);
             Math.DivRem((index2 + 4), 12, out int index3);
-            return $"{note} - {allNotes[index2]} - {allNotes[index3]}";
+            return $"{baseNote} - {allNotes[index2]} - {allNotes[index3]}";
         }
+
+
+
+        public string GeefAkkoord(string baseNote, bool mineur, bool sus, bool dominant, bool verminderd)
+        {
+            var index1 = allNotes.IndexOf(baseNote);
+            int b = 4;
+            int c = 7;
+            int d = 11;
+            if (mineur)
+            {
+                b--;
+                d--;
+            }
+            if (sus)
+            {
+                c -= 2;
+            }
+            if (dominant)
+            {
+                d -= 1;
+            }
+            if (verminderd)
+            {
+                if (!mineur)
+                {
+                    b--;
+                }
+                if (sus)
+                {
+                    c++;
+                }
+                else
+                {
+                    c--;
+                }
+                if (dominant)
+                {
+                    d--;
+                }
+                else
+                {
+                    d -= 2;
+                }
+            }
+
+
+            Math.DivRem((index1 + b), 12, out int index2);
+            Math.DivRem((index1 + c), 12, out int index3);
+            Math.DivRem((index1 + d), 12, out int index4);
+            return $"{baseNote} - {allNotes[index2]} - {allNotes[index3]} - {allNotes[index4]}";
+        }
+
     }
 }
--- PlayChordsTests/ChordTests.cs
+++ PlayChordsTests/ChordTests.cs
@@ -11,17 +11,53 @@ namespace PlayChords.Tests
     [TestClass()]
     public class ChordTests
     {
+        [TestMethod()]
+        public void MajeurAkkoordTest1()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - E - G", sut.MajeurAkkoord("C"));
+        }
+
+
+
+
+        [TestMethod()]
+        public void MineurAkkoordTest1()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - Es - G", sut.MineurAkkoord("C"));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         [DataTestMethod]
         [DataRow("C", "C - E - G")]
         [DataRow("Des", "Des - F - As")]
         [DataRow("D", "D - Ges - A")]
         [DataRow("Es", "Es - G - Bes")]
         [DataRow("As", "As - C - Es")]
-        public void MajeurAkkoordTest(string baseNote, string expected)
+        public void MajeurAkkoordTest2(string baseNote, string expected)
         {
             var sut = new Chord();
             Assert.AreEqual(expected, sut.MajeurAkkoord(baseNote));
         }
+
+
 
         [DataTestMethod]
         [DataRow("C", "C - Es - G")]
@@ -29,10 +65,68 @@ namespace PlayChords.Tests
         [DataRow("D", "D - F - A")]
         [DataRow("Es", "Es - Ges - Bes")]
         [DataRow("As", "As - B - Es")]
-        public void MineurAkkoordTest(string baseNote, string expected)
+        public void MineurAkkoordTest2(string baseNote, string expected)
         {
             var sut = new Chord();
             Assert.AreEqual(expected, sut.MineurAkkoord(baseNote));
+        }
+
+
+
+
+
+
+
+
+
+        [TestMethod()]
+        public void GeefMajAkkoordTest()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - E - G - B", sut.GeefAkkoord("C", false, false, false, false));
+        }
+
+
+
+        [TestMethod()]
+        public void GeefMinAkkoordTest()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - Es - G - Bes", sut.GeefAkkoord("C", true, false, false, false));
+        }
+
+
+
+
+        [TestMethod()]
+        public void GeefSusAkkoordTest()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - E - F - B", sut.GeefAkkoord("C", false, true, false, false));
+        }
+
+
+        [TestMethod()]
+        public void GeefDomAkkoordTest()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - E - G - Bes", sut.GeefAkkoord("C", false, false, true, false));
+        }
+
+
+        [TestMethod()]
+        public void GeefVermAkkoordTest()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - Es - Ges - A", sut.GeefAkkoord("C", false, false, false, true));
+        }
+
+
+        [TestMethod()]
+        public void GeefVermAkkoordTest2()
+        {
+            var sut = new Chord();
+            Assert.AreEqual("C - Es - Ges - A", sut.GeefAkkoord("C", false, true, true, true));
         }
 
     }
