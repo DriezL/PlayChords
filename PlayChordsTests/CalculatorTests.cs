d497d595b9f307016148f4f8ce0da875110362a5
Dries de Groot <GrootD@wigo4it.nl>
2022-02-28 14:29:32 +0100

    FsCheck toegevoegd

---
+++ PlayChordsTests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using FsCheck;
+using FsCheck.NUnit;
+
+namespace PlayChords.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class CalculatorTests
+    {
+        [Property]
+        public Property TestAdd(int a, int b)
+        {
+            var calculator = new Calculator();
+            return (calculator.Add(a, b) == calculator.Add(b, a))
+                .ToProperty();
+        }
+    }
+}
